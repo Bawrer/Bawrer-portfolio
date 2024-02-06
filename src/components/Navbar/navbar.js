21f7843cfc50356a1fb4feb809e0e150dff73e90
Bawrer <zwenibonginkosi@gmail.com>
2024-02-06 11:59:55 +0200

    navbar update

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import './navbar.css';
 import logo from '../../assets/logo.png';
+import {Link} from 'react-scroll';
 
 const Navbar = () => {
   return (
     <nav className='navbar'>
       <img src={logo} alt='logo' className='logo'/>
-      <div className='desktopMenu'>
-      </div>
+      <div className="desktopMenu">
+      
+            <Link className="desktopMenuListItem">Home</Link>
+            <Link className='desktopMenuListItem'>About</Link>
+            <Link className='desktopMenuListItem'>portfolio</Link>
+            <Link className='desktopMenuListItem'>Certifications</Link>
+            <Link className='desktopMenuListItem'>Contact</Link>
 
+      </div>
       <button className='desktopMenubtn'>
       <img className='desktopMenuImg' alt=''/> toggle button </button>
     </nav>
