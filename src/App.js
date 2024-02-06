b76507abd8076d7934bd3e59dcf5f41f0313bb78
Bawrer <zwenibonginkosi@gmail.com>
2024-02-06 16:04:53 +0200

    introduction

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import Navbar from "./components/Navbar/navbar";
+import Intro from './components/intro/intro';
+
+
 function App() {
   return (
     <div className="App">
        <Navbar/>
+       
     </div>
   )
 }
