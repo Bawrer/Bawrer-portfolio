0fb7fd1ca0610e991fbe34d98e0054e445fe2c8e
Bawrer <zwenibonginkosi@gmail.com>
2024-02-06 10:53:53 +0200

    components updated

---
+++ src/components/Navbar/navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './navbar.css';
+import logo from '../../assets/logo.png';
+
+const Navbar = () => {
+  return (
+    <nav className='navbar'>
+      <img src={logo} alt='logo' className='logo'/>
+      <div className='desktopMenu'>
+      </div>
+
+      <button className='desktopMenubtn'>
+      <img className='desktopMenuImg' alt=''/> toggle </button>
+    </nav>
+    
+  )
+}
+
+export default Navbar
