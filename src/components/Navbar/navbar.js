d38418ea5f0951f99f8a71d91b2f4e2e80afc37f
Bawrer <zwenibonginkosi@gmail.com>
2024-02-06 13:18:30 +0200

    save

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './navbar.css';
 import logo from '../../assets/logo.png';
 import {Link} from 'react-scroll';
+import toggleImg from '../'
 
 const Navbar = () => {
   return (
     <nav className='navbar'>
-      <img src={logo} alt='logo' className='logo'/>
+      <button className='desktopMenubtn'>
+      <img className='desktopMenuImg' alt=''/> toggle button </button>
       <div className="desktopMenu">
       
             <Link className="desktopMenuListItem">Home</Link>
@@ -16,8 +18,9 @@ const Navbar = () => {
             <Link className='desktopMenuListItem'>Contact</Link>
 
       </div>
-      <button className='desktopMenubtn'>
-      <img className='desktopMenuImg' alt=''/> toggle button </button>
+
+      <img src={logo} alt='logo' className='logo'/>
+     
     </nav>
     
   )
