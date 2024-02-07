3c7fa71088fdb85b01b7c447001b5f53dd330e7c
Bawrer <zwenibonginkosi@gmail.com>
2024-02-07 14:48:38 +0200

    toggle button

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,27 +1,32 @@
-import React from 'react';
-import './navbar.css';
- 
-import {Link} from 'react-scroll';
- 
+import React, { useState } from 'react';
+import './navbar.css'; // Import external CSS file
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
+  // State to track the current color
+  const [isColorOn, setIsColorOn] = useState(false);
+
+  // Function to toggle between colors
+  const toggleColor = () => {
+    setIsColorOn(prevState => !prevState);
+  };
+
   return (
-    <nav className='navbar'>
-      <button className='desktopMenubtn'>On  &  Off </button>
+    <nav className={`navbar ${isColorOn ? 'colorOn' : 'colorOff'}`}>
+      {/* Toggle button to change between two colors */}
+      <button className='desktopMenubtn' onClick={toggleColor}>
+        {isColorOn ? 'Off' : 'On'}
+      </button>
+
       <div className="desktopMenu">
-      
-            <Link className="desktopMenuListItem">Home</Link>
-            <Link className='desktopMenuListItem'>About</Link>
-            <Link className='desktopMenuListItem'>portfolio</Link>
-            <Link className='desktopMenuListItem'>Certifications</Link>
-            <Link className='desktopMenuListItem'>Contact</Link>
-
+        <Link className="desktopMenuListItem">Home</Link>
+        <Link className='desktopMenuListItem'>About</Link>
+        <Link className='desktopMenuListItem'>Portfolio</Link>
+        <Link className='desktopMenuListItem'>Certifications</Link>
+        <Link className='desktopMenuListItem'>Contact</Link>
       </div>
-
-     
     </nav>
-    
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
