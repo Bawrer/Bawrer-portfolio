1f97bc8529c47b5b47ad9b133047445d96e72146
Bawrer <zwenibonginkosi@gmail.com>
2024-02-07 12:00:33 +0200

    styling the introcduction  content

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -7,7 +7,7 @@ const Intro = () => {
     <section id='intro'>
       <div className='introContent'>
         <span className='welcome'>Welcom to my portfolio</span> <br />
-        <span className='introText'>I'm <span className='introName'>Bonginkosi</span> <br />Creativity, simplicity and accuracy is what I bring to the field as a<br/><span className='careerName'>Junior software developer</span> </span>
+        <span className='introText'>I'm <span className='introName'>Bonginkosi</span> <br /><span className='introPara'>Creativity, simplicity and accuracy is what I bring to the field as a</span><br/><span className='careerName'>Junior software developer</span> </span>
     </div>
       <img src={bg} alt='bg' className='bg' />
     </section>
