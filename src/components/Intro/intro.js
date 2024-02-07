6e2187611de31134723acb344666fd445daafc11
Bawrer <zwenibonginkosi@gmail.com>
2024-02-07 13:57:35 +0200

    Install Font Awesome

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './intro.css';
-import bg from '../../assets/IMG-20231026-WA0025-removebg-preview.png';
+import bg from '../../assets/IMG-20230509-WA0042-removebg-preview.png';
 
 const Intro = () => {
   return (
     <section id='intro'>
       <div className='introContent'>
-        <span className='welcome'>Welcom to my portfolio</span> <br />
-        <span className='introText'>I'm <span className='introName'>Bonginkosi</span> <br /><span className='introPara'>Creativity, simplicity and accuracy is what I bring to the field as a</span><br/><span className='careerName'>Junior software developer</span> </span>
+        <span className='welcome'>Welcome to my portfolio</span> <br />
+        <span className='introText'>I'm <span className='introName'> Zweni Bonginkosi</span> <br /><span className='introPara'>Creativity, simplicity and accuracy is what I bring to the field as a</span><br/><span className='careerName'>Junior software developer</span> </span>
     </div>
       <img src={bg} alt='bg' className='bg' />
     </section>
