7460d7e7dcacf5bebd13ad17c54f83f5834436e9
Bawrer <zwenibonginkosi@gmail.com>
2024-02-07 10:01:12 +0200

    inserting the introduction content and also position it using css

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './intro.css';
+import bg from '../../assets/IMG-20231026-WA0025-removebg-preview.png';
 
 const Intro = () => {
   return (
     <section id='intro'>
-    <div className='introContent'>
-
+      <div className='introContent'>
+        <span className='welcome'>Welcom to my portfolio</span> <br />
+        <span className='introText'>I'm <span className='introName'>Bonginkosi</span> <br />Creativity, simplicity and accuracy is what I bring to the field as a<br/><span className='careerName'>Junior software developer</span> </span>
     </div>
-      <img src='' alt='' className='' />
+      <img src={bg} alt='bg' className='bg' />
     </section>
   )
 }
