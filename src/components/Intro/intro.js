627b472d995ef74e77470e97a179105caa2c2648
Bawrer <zwenibonginkosi@gmail.com>
2024-02-09 16:02:01 +0200

    added a components called certficaates and create two files which css and js

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -20,8 +20,9 @@ const Intro = () => {
           <ul id={menuOpen ? 'sidemenu open' : 'sidemenu'}>
             <li><a href="#header">Home</a></li>
             <li><a href="#about">About</a></li>
-            {/* <li><a href="#services">Services</a></li> */}
+            
             <li><a href="#portfolio">Portfolio</a></li>
+            <li><a href="#Certifications">Certificates</a></li>
             <li><a href="#contact">Contact</a></li>
             <i className="fa-solid fa-xmark" onClick={closeMenu}></i>
           </ul>
