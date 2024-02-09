cd29d2bcd833d735289bcdbbbc7bbc73b54b3f9e
Bawrer <zwenibonginkosi@gmail.com>
2024-02-09 11:54:19 +0200

    my work  content updated

--- src/components/Portfolio/portfolio.js
+++ src/components/Portfolio/portfolio.js
@@ -1,73 +1,64 @@
 import React from 'react';
-import './portfolio.css';
+ import './portfolio.css'
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons';
+const WorkItem = ({ imageSrc, title, description, link }) => (
+  <div className="work">
+    <img src={imageSrc} alt={title} height="250px" />
+    <div className="layer">
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <a href={link}><i className="fa-solid fa-arrow-up-right-from-square"></i></a>
+    </div>
+  </div>
+);
 
-const Portfolio = () => {
-  const projects = [
-    {
-      id: 1,
-      title: 'Bank App',
-      description: 'Bank app that allows customers to deposit and withdraw money',
-      image: '/images/project 1.png',
-      link: '#',
-    },
-    {
-      id: 2,
-      title: 'Metric Convertor',
-      description: 'Metric converter that allows people from different parts of the world to be able to convert unit from one of to another.',
-      image: '/images/Screenshot (72).png',
-      link: '#',
-    },
-    {
-      id: 3,
-      title: 'Intro to Python',
-      description: 'This certicate shows that I have succesfully completed a course by demostrating theoretical and practical understanding of Intro to python.',
-      image: '/images/b24465f8-60b0-421d-b868-be8f9dcd508d.jpg',
-      link: 'https://www.sololearn.com/certificates/CC-HABIYLN0',
-    },
-    {
-      id: 4,
-      title: 'IBM Certificate',
-      description: 'Through self-paced learning, this badge earner has displayed an understanding of topics such as Git concepts, using command-line, advantages of using GIT, installing and running GIT on you local computer.',
-      image: '/images/git (3).png',
-      link: 'https://www.credly.com/badges/411d2f3c-c187-4eeb-8b23-cf6dfee30c86/print',
-    },
-    {
-      id: 5,
-      title: '5 Stars Group Website',
-      description: 'This is a website that we built to help students who are willing to take part on the IT industry and those who are looking for jobs as well.',
-      image: '/images/Screenshot (73).png',
-      link: '#',
-    },
-    {
-      id: 6,
-      title: 'Tax Calculator',
-      description: 'This is a tax calculator that helps people of different age groups to calculate their tax for the whole year after reductions.',
-      image: '/images/Screenshot (76).png',
-      link: '#',
-    },
-  ];
+const Portfolio = () => (
+  <div id="portfolio">
+    <div className="container">
+      <h1 className="sub-title">My Work</h1>
+      <div className="work-list">
+        <WorkItem
+          imageSrc="./assets/Screenshot (75).png"
+          title="Social Media App"
+          description="Chatbot that provides people who have questions about our website with automated responses."
+          link="#"
+        />
+        <WorkItem
+          imageSrc="../../"
+          title="Bank App"
+          description="Bank app that allows customers to deposit and withdraw money."
+          link="#"
+        />
+        <WorkItem
+          imageSrc="/assets/Screenshot (72).png"
+          title="Metric Converter"
+          description="Metric converter that allows people from different parts of the world to be able to convert units from one to another."
+          link="#"
+        />
 
-  return (
-    <div className="portfolio">
-      <h1>My Portfolio</h1>
-      <div className="projects">
-        {projects.map(project => (
-          <div className="work" key={project.id}>
-            <img src={project.image} alt="" height="250px" />
-            <div className="layer">
-              <h3>{project.title}</h3>
-              <p>{project.description}</p>
-              <a href={project.link}><i className="fa-solid fa-arrow-up-right-from-square"></i></a>
-            </div>
-          </div>
-        ))}
+<WorkItem
+          imageSrc="./images/Screenshot (75).png"
+          title="Social Media App"
+          description="Chatbot that provides people who have questions about our website with automated responses."
+          link="#"
+        />
+        <WorkItem
+          imageSrc="../../"
+          title="Bank App"
+          description="Bank app that allows customers to deposit and withdraw money."
+          link="#"
+        />
+        <WorkItem
+          imageSrc="/assets/Screenshot (72).png"
+          title="Metric Converter"
+          description="Metric converter that allows people from different parts of the world to be able to convert units from one to another."
+          link="#"
+        />
+        {/* Add more WorkItem components for additional works */}
       </div>
       <a href="https://github.com/Bawrer?tab=repositories" className="btn">See More</a>
     </div>
-  );
-};
+  </div>
+);
 
 export default Portfolio;
