537510d54288c80c648534ab10c2e42cbb9ec4bd
Bawrer <zwenibonginkosi@gmail.com>
2024-02-15 13:04:58 +0200

    Refactor: Add responsive design for contact section
    Updated CSS to ensure responsiveness of the contact section across different screen sizes. Adjusted layout and styling to maintain the original design while adapting to various devices.

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -3,9 +3,12 @@ import './intro.css';
 import video from '../../assets/Back_video (1).mp4';
  
 
-
 const Intro = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   const openMenu = () => {
     setMenuOpen(true);
@@ -37,13 +40,15 @@ const Intro = () => {
           </ul>
           <i className="fa-solid fa-bars" onClick={openMenu}></i>
         </nav>
-
+        
         <div className='social-header'>
+        
                 <div class="social-icons">
                   <a href="https://twitter.com/Bongink45395379"><i class="fa-brands fa-github"></i></a>
                   <a href="https://www.instagram.com/borah_zweni/"><i class="fa-brands fa-instagram"></i></a>
                   <a href="https://www.linkedin.com/in/bonginkosi-zweni-164794170/"><i class="fa-brands fa-linkedin"></i></a>
                </div>
+             
               <div className="header-tex">
                 <p>Fullstack software developer</p>
                 <h1>Hi, I'm <span>Bonginkosi</span><br /> Zweni From South Africa</h1>
